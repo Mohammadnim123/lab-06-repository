ba4c30cb4baea3853fe3caf3b5a00687b4359793
Student <mm.nemrawi96@gmail.com>
2020-07-11 19:34:39 +0300

    lab-9

--- server.js
+++ server.js
@@ -5,17 +5,25 @@ const pg = require('pg');
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const yelp = require('yelp-fusion');
 const client = new pg.Client(process.env.DATABASE_URL);
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 
+
+// -----------------------------------------------------------------------------------------
 app.get('/', (request, response) => {
   response.send('Home Page!');
 });
+
+// --------------------------------------------------------------------------------
 app.get('/location', locationHandler);
 app.get('/weather', weatherHandler);
 app.get('/trails', trailsHandler);
+app.get('/movies', moviesHandler);
+app.get('/yelp', yelpHandler);
+// -----------------------------------------------------------------------------------------
 
 function locationHandler(request, response) {
   const city = request.query.city;
@@ -62,6 +70,11 @@ function weatherHandler(request, response) {
       response.status(200).json(val);
     });
 }
+
+// -----------------------------------------------------------------------------------------
+
+
+
 function getWeather(latitude, longitude) {
   let weatherSummaries = [];
   let key = process.env.WEATHER_KEY;
@@ -83,6 +96,10 @@ function Weather(day) {
   this.forecast = day.weather.description;
   this.time = new Date(day.valid_date).toString().slice(0, 15);
 }
+
+// -----------------------------------------------------------------------------------------
+
+
 function trailsHandler(request, response) {
   let latitude = request.query.latitude;
   let longitude = request.query.longitude;
@@ -119,6 +136,102 @@ function Trail(trail) {
   this.condition_date = trail.conditionDate.split(" ")[0];
   this.condition_time = trail.conditionDate.split(" ")[1];
 }
+
+// -----------------------------------------------------------------------------------------
+
+
+
+function moviesHandler(request, response) {
+  let city = request.query.city;
+  getMovies(city)
+    .then(val => {
+      response.status(200).json(val);
+    });
+}
+
+function getMovies(city) {
+  let moviesArr = [];
+  let key = process.env.MOVIE_API_KEY;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${city}&language=en-US`;
+ 
+  return superagent.get(url)
+    .then(moviesData => {
+      let data = moviesData.body.results;
+      return data;
+    })
+    .then(moviesData => {
+      moviesArr = moviesData.map(val => {
+        return new Movies(val)
+      });
+      return moviesArr
+    });
+}
+
+function Movies(val){
+  this.title = val.title;
+  this.overview = val.overview;
+  this.average_votes = val.vote_average;
+  this.total_votes = val.vote_count;
+  this.image_url = `https://image.tmdb.org/t/p/w500/${val.poster_path}`;
+  this.popularity = val.popularity;
+  this.released_on = val.release_date;
+}
+
+
+// -------------------------------------------------------------------------------------------------------------------
+
+function yelpHandler(request, response) {
+  let latitude = request.query.latitude;
+  let longitude = request.query.longitude;
+  getyelp(latitude, longitude)
+    .then(val => {
+      response.status(200).json(val);
+    });
+}
+
+function getyelp(latitude, longitude) {
+  
+  let yelpArr = [];
+const apiKey = process.env.YELP_API_KEY;
+
+const searchRequest = {
+  latitude : latitude,
+  longitude : longitude
+};
+
+ 
+
+ return yelp.client(apiKey).search(searchRequest)
+.then(yelpData => {
+  const firstResult = yelpData.jsonBody;
+  let yelpFirst = firstResult.businesses;
+  return yelpFirst;
+})
+.then(yelpData => {
+  yelpArr = yelpData.map(val => {
+    return new Yelp(val)
+  });
+return yelpArr;
+
+      
+    })
+}
+
+
+
+
+function Yelp(yelp){
+  this.name = yelp.name;
+  this.image_url = yelp.image_url;
+  this.price = yelp.price;   
+  this.rating = yelp.rating;
+  this.url = yelp.url;
+}
+
+
+// -------------------------------------------------------------------------------------------------------------------
+
+
 client.connect()
   .then(() => {
     app.listen(PORT, () =>
